2f95aea0551edc9d534082cdb559843d4180c514
James Cuénod <jcuenod@users.noreply.github.com>
2022-03-17 08:35:04 -0500

    Only delete split if backing up

--- backup.sh
+++ backup.sh
@@ -4,14 +4,14 @@ echo "EXECUTING BACKUP OF `pwd`"
 
 DoBackupFlag=true
 
-rm -f zotero.sqlite.part*
-split -b 25M zotero.sqlite "zotero.sqlite.part"
 Line=$(git status | tail -n 1)
 if [ "$Line" = "nothing to commit, working tree clean" ]; then
 	DoBackupFlag=false
 fi;
 
 if $DoBackupFlag; then
+	rm -f zotero.sqlite.part*
+	split -b 25M zotero.sqlite "zotero.sqlite.part"
 	git add .
 	Pdfs=$(git status | grep -E "\.pdf$" | sed -E 's/\smodified(.*)/mod \1/' | sed -E 's/\snew(.*)/new \1/' | sed -E 's/\sdeleted(.*)/del \1/' | sed -E 's/([a-z]{3}).*\/([^\/]*.pdf)$/(\1) \2/')
 	git commit -m "Daily backup" -m "$Pdfs"
