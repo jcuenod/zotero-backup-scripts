6ebe89af6be1f78c5d8e532574466609978405a6
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2023-05-09 19:15:53 -0500

    Add support for other file formats

--- backup.sh
+++ backup.sh
@@ -13,13 +13,13 @@ if $DoBackupFlag; then
 	rm -f zotero.sqlite.part*
 	split -b 25M zotero.sqlite "zotero.sqlite.part"
 	git add .
-	Pdfs=$(git status | grep -E "\.pdf$" \
+	ChangedDocuments=$(git status | grep -E "\.(pdf|html|epub|pptx|docx)$" \
 		| sed -E 's/\smodified(.*)/mod \1/' \
 		| sed -E 's/\snew(.*)/new \1/' \
 		| sed -E 's/\srenamed(.*)/ren \1/' \
 		| sed -E 's/\sdeleted(.*)/del \1/' \
 		| sed -E 's/([a-z]{3}).*\/([^\/]*.pdf)$/(\1) \2/')
-	git commit -m "Daily backup" -m "$Pdfs"
+	git commit -m "Daily backup" -m "$ChangedDocuments"
 	git push origin master
 else
 	echo " -> Nothing to backup (no changes since last backup)"
